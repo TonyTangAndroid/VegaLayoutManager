04491526bbb934ec332de009a7c74822b309fa07
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-04-23 13:01:27 +0800

    Refactor code in MainActivity.java

---
+++ VegaLayoutManager/app/src/main/java/com/stone/vega/MyViewHolder.java
@@ -0,0 +1,36 @@
+package com.stone.vega;
+
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+
+  private final MainActivity mainActivity;
+  public TextView nameTv;
+  public TextView currentPriceTv;
+  public ImageView trendFlagIv;
+  public TextView grossTv;
+
+  public MyViewHolder(MainActivity mainActivity, View itemView) {
+    super(itemView);
+    this.mainActivity = mainActivity;
+    this.nameTv = itemView.findViewById(R.id.item_name_tv);
+    this.currentPriceTv = itemView.findViewById(R.id.item_current_price);
+    this.trendFlagIv = itemView.findViewById(R.id.item_trend_flag);
+    this.grossTv = itemView.findViewById(R.id.item_gross);
+  }
+
+  @SuppressLint("SetTextI18n")
+  public void bindData(StockEntity stockEntity) {
+    nameTv.setText(stockEntity.getName());
+    currentPriceTv.setText("$" + stockEntity.getPrice());
+    trendFlagIv
+        .setImageResource(stockEntity.getFlag() > 0 ? R.drawable.up_red : R.drawable.down_green);
+    grossTv.setText(stockEntity.getGross());
+    grossTv
+        .setTextColor(stockEntity.getFlag() > 0 ? mainActivity.redColor : mainActivity.greenColor);
+  }
+}
--- VegaLayoutManager/app/src/main/java/com/stone/vega/StockEntity.java
+++ VegaLayoutManager/app/src/main/java/com/stone/vega/StockEntity.java
@@ -6,31 +6,31 @@ package com.stone.vega;
 
 public class StockEntity {
 
-    private String name;
-    private float price;
-    private int flag;
-    private String gross;
+  private String name;
+  private float price;
+  private int flag;
+  private String gross;
 
-    public StockEntity(String name, float price, int flag, String gross) {
-        this.name = name;
-        this.price = price;
-        this.flag = flag;
-        this.gross = gross;
-    }
+  public StockEntity(String name, float price, int flag, String gross) {
+    this.name = name;
+    this.price = price;
+    this.flag = flag;
+    this.gross = gross;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public float getPrice() {
-        return price;
-    }
+  public float getPrice() {
+    return price;
+  }
 
-    public int getFlag() {
-        return flag;
-    }
+  public int getFlag() {
+    return flag;
+  }
 
-    public String getGross() {
-        return gross;
-    }
+  public String getGross() {
+    return gross;
+  }
 }
